26b141964f838218136cd196c2dc41fdb98c40dc
zhangqi <826167518@qq.com>
2017-10-09 21:33:39 -0700

    分类

--- ceshi/sishe.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-# -*- coding:utf-8 -*-
-a = input ()
-b = input ()
-
-print int(a + 0.5),int(b + 0.5)
--- easygui/choicebox.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python
-# -*- coding:utf-8 -*-
-import easygui
-flavor = easygui.choicebox("What is your favorite ice cream flavor?", choices=['Vanilla','Chocolate','Strawberry'])
-easygui.msgbox("You pickled" + flavor)
